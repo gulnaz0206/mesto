a6c1ad182dfbf2a8560e5216f2381377935ffad4
Gulnaz Nasrtdinova <gulnaz0206@yandex.ru>
2023-01-13 16:55:47 +0500

    edit after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,33 @@
-const popupElement = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup');
 const popupOpenButtonElement = document.querySelector('.profile__edit');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupCloseButtonElement = popupProfile.querySelector('.popup__close-button');
+const popupProfileForm = popupProfile.querySelector('.popup__form');
+const popupInputName = popupProfileForm.querySelector('.popup__input_name');
+const popupInputJob = popupProfileForm.querySelector('.popup__input_job');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const popupSubmitButton = popupProfile.querySelector('.popup__submit-button');
 
-const openPopup = function (event) {
-    popupElement.classList.add('popup_opened');
-    console.log('Open popup clicked');
+const openPopup = function (popup) {
+    popup.classList.add('popup_opened');
 }
 
-const closePopup = function () {
-    popupElement.classList.remove('popup_opened');
+const closePopup = function (popup) {
+    popup.classList.remove('popup_opened');
 }
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
+function submitProfileInfo (event) {
+    event.preventDefault();
+    profileName.textContent = popupInputName.value;
+    profileJob.textContent = popupInputJob.value;
+    closePopup(popupProfile);
+}
+
+popupOpenButtonElement.addEventListener('click', function() {
+     openPopup(popupProfile);
+    popupInputName.value = profileName.textContent;
+    popupInputJob.value = profileJob.textContent;
+    });
+popupCloseButtonElement.addEventListener('click', function() {
+    closePopup(popupProfile)});
+popupProfileForm.addEventListener('submit', submitProfileInfo);
